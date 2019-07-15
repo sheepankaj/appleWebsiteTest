a1f44ae881878563de98306e7ab7d5304efbf2b7
Pankaj Kumar Shee <sheepankaj@gmail.com>
2019-07-15 10:57:12 +0100

    Tv Tab test script

---
+++ appleWebsiteTest/src/com/testing/applewebsite/TVTab.java
@@ -0,0 +1,30 @@
+package com.testing.applewebsite;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class TVTab {
+	
+	public void tvTab(WebDriver driver) throws InterruptedException {
+		if(driver!=null) {
+		driver.findElement(By.xpath("//*[@id=\"ac-globalnav\"]/div/ul[2]/li[6]/a")).click();
+		}else {
+			System.out.println("Driver null" + driver);
+		}
+		
+		
+		String actualTabTitle ="TV - Apple (IE)";
+		String expectedTabTitile = "";
+		actualTabTitle = driver.getTitle();
+		expectedTabTitile = driver.getTitle();
+		
+		if(actualTabTitle.contentEquals(expectedTabTitile)) {
+			System.out.println("TV Tab link clicked");
+		} else {
+			System.out.println("Nothing Found");
+		}
+		
+		driver.navigate().back();
+	}
+
+}
